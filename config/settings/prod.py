f7886478258916a3d4c9fcdf33d44e2540c3b125
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-25 19:22:42 +0900

    db삭제

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,13 +5,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'emsys',
-        'USER': 'dbmasteruser',
-        'PASSWORD': 'nx%K;b)2]Dx+otNC&ah,LBoB|82d1ghP',
-        'HOST': 'ls-e7085ef971e2ab19fa06e7e909875e07e8c80e2e.cnibbhagbaiv.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
 }
