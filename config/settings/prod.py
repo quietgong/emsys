189c77d3e1b9f5c9af96799069a14f6034cd7829
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-25 17:21:47 +0900

    DB설정 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['3.34.213.119', 'emsys.site']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'nx%K;b)2]Dx+otNC&ah,LBoB|82d1ghP',
+        'HOST': 'ls-e7085ef971e2ab19fa06e7e909875e07e8c80e2e.cnibbhagbaiv.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
