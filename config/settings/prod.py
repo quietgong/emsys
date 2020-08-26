6e1f65f1bbf46f22f78876dbba0c7c9f4f58dada
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-26 15:37:52 +0900

    img load

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,9 @@
 from .base import *
 
 ALLOWED_HOSTS = ['15.164.187.80', 'emsys.site']
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = []
 DEBUG = False
+
+MEDIA_ROOT = '/home/ubuntu/projects/mysite/media/'
+MEDIA_URL = '/media/'
