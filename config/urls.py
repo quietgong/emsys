8cd415523e9a0a614b867fd26e263c88bf5a06e2
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-26 15:16:24 +0900

    urls 제거

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,6 @@ from emsys.views import base_views
 urlpatterns = [
     path('emsys/', include('emsys.urls')),
     path('common/', include('common.urls')),
-    path('media/', include('media.urls')),
     path('admin/', admin.site.urls),
     path('', base_views.index, name='index'), # '/' 에 해당하는 path
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
