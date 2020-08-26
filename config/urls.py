7463da2dc1df8cf552175bd9453e0ca17b203375
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-26 15:57:42 +0900

    prod 수정

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.static import serve
 from emsys.views import base_views
 
 urlpatterns = [
@@ -11,4 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', base_views.index, name='index'), # '/' 에 해당하는 path
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 handler404 = 'common.views.page_not_found'
