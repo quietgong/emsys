4f0c4ba5c376e41acc32a45b55df6cd2d8004e1f
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-26 16:13:13 +0900

    prod 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,14 +2,3 @@ from .base import *
 
 ALLOWED_HOSTS = ['15.164.187.80', 'emsys.site']
 DEBUG = False
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static") #개발시 스태틱파일을 모아서 복사해줄 디렉토리
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static", "media")
-
-STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(BASE_DIR),"static", "static_dirs"),
-    os.path.join(os.path.dirname(BASE_DIR),"static", "media"),
-]
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from emsys.views import base_views
 
@@ -11,4 +12,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', base_views.index, name='index'), # '/' 에 해당하는 path
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^%s(?P.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), serve,
+            kwargs={'document_root': settings.STATIC_ROOT})
+]
+
 handler404 = 'common.views.page_not_found'
