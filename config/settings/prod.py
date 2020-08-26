3bcbf2e32346c651c5e8744b6488fcf88617c65c
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-26 15:51:04 +0900

    prod 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,15 +1,13 @@
 from .base import *
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROD_DIR = os.path.dirname(os.path.dirname(__file__))
 
 ALLOWED_HOSTS = ['15.164.187.80', 'emsys.site']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = []
 DEBUG = False
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = (BASE_DIR)
+MEDIA_ROOT = (PROD_DIR)
 MEDIA_URL = '/media/'
