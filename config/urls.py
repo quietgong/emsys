1b412504b2473f22d5f46b22b9b45448d210fec8
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-08-26 16:17:03 +0900

    prod 수정

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,19 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.views.static import serve
 
 from emsys.views import base_views
 
 urlpatterns = [
-    path('emsys/', include('emsys.urls')),
-    path('common/', include('common.urls')),
-    path('admin/', admin.site.urls),
-    path('', base_views.index, name='index'), # '/' 에 해당하는 path
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-urlpatterns += [
-    re_path(r'^%s(?P.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), serve,
-            kwargs={'document_root': settings.STATIC_ROOT})
-]
+                  path('emsys/', include('emsys.urls')),
+                  path('common/', include('common.urls')),
+                  path('admin/', admin.site.urls),
+                  path('', base_views.index, name='index'), # '/' 에 해당하는 path
+                  url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+                  url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+              ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 handler404 = 'common.views.page_not_found'
